e7d74fa65b0a7d18a4c3f2a51eb68b2319a173bc
Clinton <soares.clinton@gmail.com>
2019-01-08 00:28:01 +0530

    --> Basic Navigation Drawer Completed --> Designing to be done

--- app/src/main/java/com/vehiclerental/app/vehiclerental/HomeActivity.java
+++ app/src/main/java/com/vehiclerental/app/vehiclerental/HomeActivity.java
@@ -1,13 +1,43 @@
 package com.vehiclerental.app.vehiclerental;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+
+                        // close drawer when item is tapped
+                        drawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                }
+        );
+
     }
 }
